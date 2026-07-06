c3bef2d85ee8f275d87284b2a52ed00b200f742c
waynewonder1 <wayne.wonder@student.moringaschool.com>
2026-07-06 23:08:24 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = Date.today().toString('7/6/2026');
+
+  return combinedObject;
+}
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
 
 
 module.exports = {
